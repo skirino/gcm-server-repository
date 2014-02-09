3c8123d2e6ba130db761bbe00630c2fdcee70fb0
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2014-02-10 02:49:03 +0900

    Add newer jar generated from http://code.google.com/p/gcm/ 7f647288103bac2c5552af881f0e217c5b95d78a

--- deployer/gcm.sh
+++ deployer/gcm.sh
@@ -1,7 +1,1 @@
-
-
-# gcm-server
-mvn install:install-file -Dmaven.repo.local=../releases -Dfile=gcm-server.jar -Dsources=gcm-server-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-server -Dversion=1.0.2 -DpomFile=gcm.pom -Dpackaging=jar -DcreateChecksum=true
-
-# gcm-client
-mvn install:install-file -Dmaven.repo.local=../releases -Dfile=gcm-client.jar -Dsources=gcm-client-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-client -Dversion=1.0.2 -Dpackaging=jar -DcreateChecksum=true
+mvn install:install-file -Dmaven.repo.local=../snapshots -Dfile=gcm-server.jar -Dsources=gcm-server-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-server -Dversion=1.0.2-SNAPSHOT -DpomFile=gcm.pom -Dpackaging=jar -DcreateChecksum=true
